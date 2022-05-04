24a4c3064f2e38cecf999adc580257f16d2f530f
TheiLLeniumStudios <104288623+TheiLLeniumStudios@users.noreply.github.com>
2022-05-04 12:05:18 +0000

    Fix extra lib args not working

--- generate-rc.ts
+++ generate-rc.ts
@@ -130,7 +130,7 @@ fetchAllNatives().then(natives => {
     .replace("%%CLIENT_GLOBALS%%", natives.client.map(s => `'${s}'`).join(", "))
 
   let extraLibs = ""
-  const extraLibUserArg = process.argv[3]
+  const extraLibUserArg = process.argv[2]
   if (extraLibUserArg?.length) {
     extraLibs = `+${extraLibUserArg}`
   }
